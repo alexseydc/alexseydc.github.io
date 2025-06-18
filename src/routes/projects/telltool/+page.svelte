658cb69d5c6aad69170147f1e472b72f1baf9a94
Alexsey daCosta <alexsey@fnkdc.com>
2025-06-17 21:17:29 -0400

    added page title and added text to the TELL Tool Page

--- src/routes/projects/telltool/+page.svelte
+++ src/routes/projects/telltool/+page.svelte
@@ -1,5 +1,18 @@
 <script>
+    import { layoutState } from '$lib/store.js';
+
+    layoutState.set({ message: 'TELL Tool Visual Identity and Branding' });
 
 </script>
 
-<div></div>
+<div>
+
+    <div class="flex justify-evenly">
+        <div class="w-400">
+            nose image
+        </div>
+        <div>
+            This project was for my Visual Identity and Branding class at the University of Michigan. The goal was for each student to develop a branding identity for Dr. Hershberger’s project, TELL Tool, at the University of Michigan’s School of Nursing. The client specified that they wanted to include certain colors, like the use of purple for fertility, and an emphasis on inclusivity. For the final project, I created a presentation for the client, a visual identity and branding guide, and an assortment of collateral so the client could see the branding identity applied in different ways. 
+        </div>
+    </div>
+</div>
