6b00da0ce127821a977febfc1bdc6540cdcc4b71
Flavio daCosta <flav@binaryservice.com>
2025-06-21 17:33:10 -0400

    tell tool logo

--- src/routes/projects/telltool/+page.svelte
+++ src/routes/projects/telltool/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import LogoImage from '$lib/assets/projects/telltool/TELLTOOL_Logo.png'
+
     import { layoutState } from '$lib/store.js';
 
     layoutState.set({ message: 'TELL Tool Visual Identity and Branding' });
@@ -8,8 +10,10 @@
 <div>
 
     <div class="flex justify-evenly">
-        <div class="w-400">
-            nose image
+        <div class="w-500 pt-5 px-10">
+            <img src={LogoImage} alt="logo"
+                class="w-64 md:w-96 lg:w-full h-auto"
+            />
         </div>
         <div class="font-lexend">
             This project was for my Visual Identity and Branding class at the University of Michigan. The goal was for each student to develop a branding identity for Dr. Hershberger’s project, TELL Tool, at the University of Michigan’s School of Nursing. The client specified that they wanted to include certain colors, like the use of purple for fertility, and an emphasis on inclusivity. For the final project, I created a presentation for the client, a visual identity and branding guide, and an assortment of collateral so the client could see the branding identity applied in different ways. 
