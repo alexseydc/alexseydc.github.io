bcd81b81c30eeb4c7c1fd53771cb8474cfd2b88a
Flavio daCosta <flav@binaryservice.com>
2025-06-25 18:51:12 -0400

    moar pages

--- src/routes/projects/botanical/+page.svelte
+++ src/routes/projects/botanical/+page.svelte
@@ -1,5 +1,29 @@
-<script>
+<script lang="ts">
+	import ProjectPage from '$lib/ProjectPage.svelte';
 
+	import branding from '$lib/assets/projects/botanical/matthaeibranding_2.png';
+	import branding01 from '$lib/assets/projects/botanical/matthaei branding-01.jpg';
+	import branding03 from '$lib/assets/projects/botanical/matthaei branding-03.jpg';
+	import branding04 from '$lib/assets/projects/botanical/matthaei branding-04.jpg';
+	import sketches from '$lib/assets/projects/botanical/matthaei_sketches.jpg';
+
+	const params = {
+		title: 'Matthaei Botanical Gardens Redesign',
+		logoImage: sketches,
+		images: [
+			{ src: branding, alt: 'branding' },
+			{ src: branding01, alt: 'branding01' },
+			{ src: branding03, alt: 'branding03' },
+			{ src: branding04, alt: 'branding04' }
+		]
+	};
 </script>
 
-<div></div>
+<ProjectPage {...params}>
+	For my Poster Design class at the University of Michigan my professor had us redesign the Matthaei
+	Botanical Garden’s visual identity. This consisted of a poster, banner, and business cards. For my
+	design, I was experimenting with something more modular that could be used as a border or placed
+	on merchandise. I wanted to be able to take apart the design and it to still work and be complete.
+	Each of the flowers in the poster are sketches of the flowers that are in the greenhouse at the
+	botanical gardens.
+</ProjectPage>
--- src/routes/projects/cards/+page.svelte
+++ src/routes/projects/cards/+page.svelte
@@ -1,5 +1,25 @@
-<script>
+<script lang="ts">
+	import ProjectPage from '$lib/ProjectPage.svelte';
 
+	import CoverImage from '$lib/assets/projects/cards/CARDS_CoverImage.png';
+	import cards1 from '$lib/assets/projects/cards/cards1.jpg';
+	import flat from '$lib/assets/projects/cards/cards_flat.png';
+
+	const params = {
+		title: 'Playing Cards Project',
+		logoImage: CoverImage,
+		images: [
+			{ src: CoverImage, alt: 'CoverImage' },
+			{ src: cards1, alt: 'cards1' },
+			{ src: flat, alt: 'flat' }
+		]
+	};
 </script>
 
-<div></div>
+<ProjectPage {...params}>
+	This project I made during a beginner illustrator course. It started out as the first set of cards
+	in red at the bottom and then I wanted to take it and rework what I had already done. This project
+	was when i really started exploring connecting graphic design to something I was really passionate
+	about, wildlife and animals. When I realized I could create things that someone could potentially
+	use and interact with it really inspired me and built my passion for graphic design.
+</ProjectPage>
