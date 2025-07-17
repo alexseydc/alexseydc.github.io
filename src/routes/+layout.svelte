07f7bcc65a2e4e0063c13b9df6bd4517b8505670
Flavio daCosta <flav@binaryservice.com>
2025-07-17 18:45:46 -0400

    prettier

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,113 +1,130 @@
 <script lang="ts">
 	import '../app.css';
-	import Gallery from '$lib/Gallery.svelte';
-    import { slide } from 'svelte/transition';
-    import { bounceInOut, bounceIn, bounceOut, cubicInOut, backInOut, sineInOut } from 'svelte/easing';
-    import { afterNavigate } from '$app/navigation';
-    import { base } from '$app/paths';
-    import Logo from '$lib/assets/LEXLOGO-01.svg'
-    import { layoutState } from '$lib/store.js';
-    import Instagram from "$lib/assets/instagram.svg";
-    import LinkedIn from "$lib/assets/linkedin.svg";
+	import { slide } from 'svelte/transition';
+	import { cubicInOut } from 'svelte/easing';
+	import { afterNavigate } from '$app/navigation';
+	import { base } from '$app/paths';
+	import Logo from '$lib/assets/LEXLOGO-01.svg';
+	import { layoutState } from '$lib/store.js';
+	import Instagram from '$lib/assets/instagram.svg';
+	import LinkedIn from '$lib/assets/linkedin.svg';
 	import NavItem from '$lib/NavItem.svelte';
 
-    let isDropdownHidden = $state(true);
+	let isDropdownHidden = $state(true);
 
-    function toggleDropdown() {
-        isDropdownHidden = !isDropdownHidden;
-        console.log('dropdown triggered')
-    }
+	function toggleDropdown() {
+		isDropdownHidden = !isDropdownHidden;
+		console.log('dropdown triggered');
+	}
 
-    // Resets mobile nav dropdown on navigation
-    afterNavigate(() => {
-        isDropdownHidden = true;
-    })
+	// Resets mobile nav dropdown on navigation
+	afterNavigate(() => {
+		isDropdownHidden = true;
+	});
 
 	let { children } = $props();
 </script>
 
-<div class="lg:p-5 p-3 bg-amber-50 min-h-screen">
-    <div class="border-2 border-slate-800 lg:px-20 lg:py-12 px-5">
-        <!-- desktop -->
-        <div class="hidden lg:flex justify-between w-full mb-20">
-            <div class="w-1/4 ">
-            <a href="{`${base}/`}" class="flex gap-x-10 group">
-                <img src={Logo} alt="logo" class="w-16 h-16"/>
-                <div class="">
-                    <span class="
+<div class="min-h-screen bg-amber-50 p-3 lg:p-5">
+	<div class="border-2 border-slate-800 px-5 lg:px-20 lg:py-12">
+		<!-- desktop -->
+		<div class="mb-20 hidden w-full justify-between lg:flex">
+			<div class="w-1/4">
+				<a href={`${base}/`} class="group flex gap-x-10">
+					<img src={Logo} alt="logo" class="h-16 w-16" />
+					<div class="">
+						<span
+							class="
+                        font-header
                         text-xl
-                        lg:text-2xl
-                        xl:text-3xl
+                        font-medium
                         text-slate-800
 
                         transition-all
-                        font-header
-                        font-medium
                         group-hover:font-bold
-                        ">Alexsey daCosta</span>
-                    <div class="w-full mt-4 border-b-2 border-slate-800 group-hover:w-[150%] transition-all"></div>
-                </div>
-            </a>
-            </div>
-            <div class="">
-              <div class="flex flex-wrap justify-end gap-5">
-                <NavItem name={"Home"} href={`${base}/`} />
-                <NavItem name={"My work"} href={`${base}/portfolio`} />
-                <NavItem name={"About me"} href={`${base}/about-me`} />
-              </div>
-            </div>
-        </div>
+                        lg:text-2xl
+                        xl:text-3xl
+                        ">Alexsey daCosta</span
+						>
+						<div
+							class="mt-4 w-full border-b-2 border-slate-800 transition-all group-hover:w-[150%]"
+						></div>
+					</div>
+				</a>
+			</div>
+			<div class="">
+				<div class="flex flex-wrap justify-end gap-5">
+					<NavItem name={'Home'} href={`${base}/`} />
+					<NavItem name={'My work'} href={`${base}/portfolio`} />
+					<NavItem name={'About me'} href={`${base}/about-me`} />
+				</div>
+			</div>
+		</div>
 
-        <!-- mobile -->
-        <div class="lg:hidden flex justify-between mt-2 mb-8 pb-2 border-b border-slate-800">
-            <div>
-                <a href="{`${base}/`}" class="flex items-center gap-x-10 group">
-                    <img src={Logo} alt="logo" class="w-10 h-10"/>
-                    <span class="col-span-3 text-xl text-slate-800 font-header font-medium">Alexsey<br />daCosta</span>
-                </a>
-            </div>
-            <button onclick={toggleDropdown} class="pl-5 cursor-pointer text-xl text-slate-800">
-                {#if isDropdownHidden}
-                    <!-- <i class="text-slate-800 text-xl fa-solid fa-bars"></i> -->
-                     <span class="text-slate-800 text-5xl">+</span>
-                {:else}
-                    <!-- <i class="text-slate-800 text-xl fa-solid fa-xmark"></i> -->
-                    <span class="text-slate-800 text-5xl inline-block transform rotate-45">+</span>
-                {/if}
-            </button>
-        </div>
-        {#if !isDropdownHidden}
-            <div transition:slide={{duration: 500, easing:cubicInOut}}
-             class="grid grid-cols-1 border-b-2 border-slate-800 mb-5">
-                <a href="{`${base}/`}" class="mx-2 mt-2 pb-2 border-b border-slate-800 text-xl font-header font-medium">Home</a>
-                <a href="{`${base}/portfolio`}" class="mx-2 mt-2 pb-2 border-b border-slate-800 text-xl font-header font-medium">My work</a>
-                <a href="{`${base}/about-me`}" class="mx-2 mt-2 pb-10  text-xl font-header font-medium">About me</a>
-            </div>
-        {/if}
+		<!-- mobile -->
+		<div class="mt-2 mb-8 flex justify-between border-b border-slate-800 pb-2 lg:hidden">
+			<div>
+				<a href={`${base}/`} class="group flex items-center gap-x-10">
+					<img src={Logo} alt="logo" class="h-10 w-10" />
+					<span class="font-header col-span-3 text-xl font-medium text-slate-800"
+						>Alexsey<br />daCosta</span
+					>
+				</a>
+			</div>
+			<button onclick={toggleDropdown} class="cursor-pointer pl-5 text-xl text-slate-800">
+				{#if isDropdownHidden}
+					<!-- <i class="text-slate-800 text-xl fa-solid fa-bars"></i> -->
+					<span class="text-5xl text-slate-800">+</span>
+				{:else}
+					<!-- <i class="text-slate-800 text-xl fa-solid fa-xmark"></i> -->
+					<span class="inline-block rotate-45 transform text-5xl text-slate-800">+</span>
+				{/if}
+			</button>
+		</div>
+		{#if !isDropdownHidden}
+			<div
+				transition:slide={{ duration: 500, easing: cubicInOut }}
+				class="mb-5 grid grid-cols-1 border-b-2 border-slate-800"
+			>
+				<a
+					href={`${base}/`}
+					class="font-header mx-2 mt-2 border-b border-slate-800 pb-2 text-xl font-medium">Home</a
+				>
+				<a
+					href={`${base}/portfolio`}
+					class="font-header mx-2 mt-2 border-b border-slate-800 pb-2 text-xl font-medium"
+					>My work</a
+				>
+				<a href={`${base}/about-me`} class="font-header mx-2 mt-2 pb-10 text-xl font-medium"
+					>About me</a
+				>
+			</div>
+		{/if}
 
-        <div class="flex items-center mb-15">
-        <div class="md:text-xl text-lg font-header text-nowrap">{$layoutState.message}</div> <div class="ml-10 w-full border-b-2 border-slate-800"></div>
-        </div>
-        {@render children()}
+		<div class="mb-15 flex items-center">
+			<div class="font-header text-lg text-nowrap md:text-xl">{$layoutState.message}</div>
+			<div class="ml-10 w-full border-b-2 border-slate-800"></div>
+		</div>
+		{@render children()}
 
-        <!-- footer -->
-        <div class="w-full">
-            <div class="w-full mt-4 border-b-2 border-slate-800 group-hover:w-[150%] transition-all"></div>
-            <div class="flex justify-between items-center my-5">
-                <div class="flex gap-2">
-                    <a href="https://www.instagram.com/alexseydc/" target="_blank">
-                        <img class="h-8" alt="Instagram" src={Instagram}/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/alexsey-dacosta-8a18b12b1/" target="_blank">
-                        <img class="h-8" alt="LinkedIn" src={LinkedIn}/>
-                    </a>
-                </div>
-                <a href="{`${base}/`}" class="">
-                    <img src={Logo} alt="logo" class="w-10 h-10"/>
-                </a>
-            </div>
-        </div>
-
-    </div>
+		<!-- footer -->
+		<div class="w-full">
+			<div
+				class="mt-4 w-full border-b-2 border-slate-800 transition-all group-hover:w-[150%]"
+			></div>
+			<div class="my-5 flex items-center justify-between">
+				<div class="flex gap-2">
+					<a href="https://www.instagram.com/alexseydc/" target="_blank">
+						<img class="h-8" alt="Instagram" src={Instagram} />
+					</a>
+					<a href="https://www.linkedin.com/in/alexsey-dacosta-8a18b12b1/" target="_blank">
+						<img class="h-8" alt="LinkedIn" src={LinkedIn} />
+					</a>
+				</div>
+				<a href={`${base}/`} class="">
+					<img src={Logo} alt="logo" class="h-10 w-10" />
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
-
